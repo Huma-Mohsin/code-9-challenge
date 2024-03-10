15760d6b7573f441c5c4148f220a1fc39f21245d
Huma-Mohsincls <you@example.com>
2024-03-10 23:05:52 +0500

    first commit

---
+++ question-1/index.js
@@ -0,0 +1,10 @@
+// Question 25: Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+// • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+// • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+// Explain & TIP: Use an if statement to check the alien's color. This introduces you to simple conditionals.
+var alien_color = "green";
+// if (alien_color ==="green"){
+// console.log("the player just earned 5 points.")
+// }
+if (alien_color === "blue") {
+}
---
+++ question-2/index.ts
@@ -0,0 +1,26 @@
+// Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+// • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+
+// • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+
+// • Write one version of this program that runs the if block and another that runs the else block.
+
+let alien_color:string="green";
+
+if(alien_color==="green"){
+    console.log("the player just earned 5 points for shooting the alien.")
+}
+else (alien_color!=="green")
+{
+    console.log("the player just earned 10 points.")
+}
+
+
+// • Write one version of this program that runs the if block and another that runs the else block.
+let alien="indigo";
+if(alien==="indigo"){
+    console.log("you look weird")
+}
+else {
+    console.log("you are beautiful.")
+}
